e4b7e28bbebdc27e049b4123c11c322d142b51ea
Donald M <dmellenbruch@outlook.com>
2019-04-10 22:54:59 -0500

    Installed additional packages, drivers, and odbc configs

--- rshiny_rstudio/config/start.sh
+++ rshiny_rstudio/config/start.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# setup $USER now
+mv -f /etc/cont-init.d/userconf /tmp/userconf.sh
+chmod +x /tmp/userconf.sh
+source /tmp/userconf.sh
+
 # Do this to ensure 'SHINYPROXY_USERNAME' and 'SHINYPROXY_GROUPS'
 # are available in the rstudio user's environment.
 env | grep "SHINYPROXY" > "/home/${USER}/.Renviron"
@@ -9,7 +14,7 @@ echo "[user]
     name = ${USER}
     email = none@none.com" > "/home/${USER}/.gitconfig"
 
-# make shiny-examples available as read-only.
+# make shiny-examples available; read-only.
 ln -sf /srv/shiny-server/ "/home/${USER}/shiny-examples"
 
 if [ "$1" != "rstudio" ]; then
@@ -19,8 +24,9 @@ else
 	# don't load shiny; load rstudio.
 	sudo rm -rf /etc/services.d/shiny-server
 
+	# if none exists, init git repo.
 	if [ ! -d "/home/${USER}/__ShinyStudio__/.git" ]; then
-		git init "/home/${USER}/__ShinyStudio__/.git"
+		git init "/home/${USER}/__ShinyStudio__"
 	fi
 fi
 
