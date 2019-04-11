510a5939b154e8a8734d7b584e7a046df70954ac
Donald M <dmellenbruch@outlook.com>
2019-04-10 22:54:06 -0500

    Update .gitignore and remove.sh

--- controls/remove.sh
+++ controls/remove.sh
@@ -1,10 +1,11 @@
 
-read -p "Permanently remove all content and preferences at '${MOUNTPOINT}'?" -n 1 -r
+read -p "Permanently remove all images, containers, content, and preferences at '${MOUNTPOINT}'?" -n 1 -r
 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 	source ./stop.sh
 	echo "*** Removing"
 	docker-compose down -v
+	docker rmi $(docker image ls --filter=reference='shinystudio*' -q)
 	sudo rm -rf "${MOUNTPOINT}"
 fi
